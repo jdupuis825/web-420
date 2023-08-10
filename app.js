81747653a7de1fea7536e79c50bb578fb1ebcc1f
Jocelyn <jdupuis@my365.bellevue.edu>
2023-08-10 16:29:10 -0400

    commit

---
+++ app.js
@@ -0,0 +1,8 @@
+/*
+============================================
+; Title: app.js
+; Author: Jocelyn Dupuis
+; Date: 08/10/2023
+; Description: App file for project
+============================================
+*/
